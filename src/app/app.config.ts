acaa3aa0ad1eab311875ed2a5d9788d446d196df
arielGs_Dev <arielogavilanes1534@gmail.com>
2025-05-26 14:53:26 -0500

    feat(config): add primeng and primeflex configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withFetch,
+} from '@angular/common/http';
 import { authInterceptor } from './core/interceptor/auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withFetch(), // Añade esta línea
       withInterceptors([authInterceptor])
-    )
-  ]
+    ),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {}, //TODO: Configurar tema de se necesario
+    }),
+  ],
 };
