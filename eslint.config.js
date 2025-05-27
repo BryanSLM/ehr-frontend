c789e05797f35f77f3e714c35d04c7b6a33793d2
arielGs_Dev <arielogavilanes1534@gmail.com>
2025-05-26 20:44:23 -0500

    feat(config): add eslint-prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettierPlugin = require('eslint-plugin-prettier');
+const angularPlugin = require('@angular-eslint/eslint-plugin');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: {
+      prettier: prettierPlugin,
+      '@angular-eslint': angularPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          singleQuote: true,
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+);
