ad6a39f1861afc23c743b492cd29c4030e58fa97
ELVIS1230 <vladimirortiz1230@gmail.com>
2025-06-17 21:30:28 -0500

    feat: avances agendamiento de cita

--- src/app/features/citas/cita-new/cita-new.component.ts
+++ src/app/features/citas/cita-new/cita-new.component.ts
@@ -8,6 +8,7 @@ import { StepsModule } from 'primeng/steps';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
 import { HorarioConsultorio } from '../horario/cita-horario-selector/cita-horario-selector.component';
+import { PatientService } from '../../../core/services/patient.service';
 
 @Component({
   selector: 'app-cita-new',
@@ -26,11 +27,12 @@ import { HorarioConsultorio } from '../horario/cita-horario-selector/cita-horari
   styleUrl: './cita-new.component.css',
 })
 export class CitaNewComponent implements OnInit {
+  constructor(private patientService: PatientService) {}
   identification = '';
-  selectedTypeIdentity = 'ci' as string | undefined;
+  selectedTypeIdentity = 'cedula' as string | undefined;
   typesIdentity = [
-    { name: 'Cedula de identidad', value: 'ci' },
-    { name: 'Pasaporte', value: 'passport' },
+    { name: 'Cedula de identidad', value: 'cedula' },
+    { name: 'Pasaporte', value: 'pasaporte' },
   ];
   typesGender = [
     { name: 'Masculino', value: 'male' },
@@ -38,21 +40,31 @@ export class CitaNewComponent implements OnInit {
   ];
   existUser = false;
   modeRegister = false;
-  currentStep = 2;
+  currentStep = 0;
   formRegister = {
-    names: '',
     identification: '',
+    names: '',
     lastNames: '',
     email: '',
     phone: '',
     birthdate: '',
     gender: '',
   };
+
   formAppointment = {
     date: new Date('2025-06-09'),
     time: '',
     doctor: '',
-    consultorio: '',
+    specialty: '',
+  };
+  user: any = {
+    names: 'Juan',
+    lastNames: 'Perez',
+    identification: '123456789',
+    email: 'vladimirortiz1230@gmail.com',
+    phone: '0987654321',
+    birthdate: '1990-01-01',
+    gender: 'male',
   };
   doctors = [
     {
@@ -103,6 +115,17 @@ export class CitaNewComponent implements OnInit {
       ] as HorarioConsultorio[],
     },
   ];
+  specialties = [
+    { name: 'Cardiología', value: 'cardiology' },
+    { name: 'Pediatría', value: 'pediatrics' },
+    { name: 'Dermatología', value: 'dermatology' },
+    { name: 'Ginecología', value: 'gynecology' },
+    { name: 'Oftalmología', value: 'ophthalmology' },
+    { name: 'Odontología', value: 'dentistry' },
+    { name: 'Traumatología', value: 'traumatology' },
+    { name: 'Psiquiatría', value: 'psychiatry' },
+    { name: 'Neurología', value: 'neurology' },
+  ];
   nextStep() {
     if (this.currentStep == 0 && !this.existUser) {
       this.modeRegister = true;
@@ -112,7 +135,60 @@ export class CitaNewComponent implements OnInit {
   }
   imprimir() {
     console.log(this.selectedTypeIdentity);
-    // console.log(this.calcularHorasCitas());
+  }
+  consultarUsuario(identification: string) {
+    this.patientService
+      .getPatientByIdentification(this.selectedTypeIdentity!, identification)
+      .subscribe({
+        next: (response) => {
+          console.log('Usuario encontrado:', response);
+          this.user = response;
+          this.existUser = true;
+
+          if (identification === '1755449004') {
+            this.formRegister = { ...this.user };
+          } else {
+            this.formRegister = {
+              names: '',
+              identification: '',
+              lastNames: '',
+              email: '',
+              phone: '',
+              birthdate: '',
+              gender: '',
+            };
+          }
+          this.nextStep();
+        },
+        error: (error) => {
+          console.error('Error al cargar pacientes:', error);
+          this.existUser = false;
+          this.formRegister = {
+            names: '',
+            identification: '',
+            lastNames: '',
+            email: '',
+            phone: '',
+            birthdate: '',
+            gender: '',
+          };
+          this.nextStep();
+        },
+      });
+  }
+  crearUsuario() {
+    console.log('Creando usuario con los siguientes datos:', this.formRegister);
+    // Aquí se podría hacer una llamada a un servicio para crear el usuario
+    this.existUser = true; // Simulamos que el usuario fue creado exitosamente
+    this.user = { ...this.formRegister }; // Actualizamos el usuario con los datos del formulario
+    this.nextStep();
+  }
+  seleccionarCita() {
+    console.log(
+      'Seleccionando cita con los siguientes datos:',
+      this.formAppointment,
+    );
+    this.nextStep();
   }
   // calcularHorasCitas(fecha) {}
 
