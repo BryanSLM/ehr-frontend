08cdaaaeae73163485a22c327e5d7657899a0e83
ELVIS1230 <vladimirortiz1230@gmail.com>
2025-06-22 23:42:17 -0500

    fix: Eliminacion de prefix /api en rutas

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -80,7 +80,7 @@ export class UserService {
     return this.http
       .get<
         ApiResponse<User[]>
-      >(`${this.apiUrl}/api/admin/users`, { headers: this.getHeaders() })
+      >(`${this.apiUrl}/admin/users`, { headers: this.getHeaders() })
       .pipe(
         tap((response) => console.log('Respuesta del servidor:', response)),
         map((response) => {
@@ -134,7 +134,7 @@ export class UserService {
     return this.http
       .post<
         ApiResponse<User>
-      >(`${this.apiUrl}/api/admin/users/create`, userData, { headers: this.getHeaders() })
+      >(`${this.apiUrl}/admin/users/create`, userData, { headers: this.getHeaders() })
       .pipe(
         tap((response) => {
           console.log('Usuario creado exitosamente:', response);
@@ -151,7 +151,7 @@ export class UserService {
     return this.http
       .patch<
         ApiResponse<User>
-      >(`${this.apiUrl}/api/admin/users/${userId}/toggle-status`, {}, { headers: this.getHeaders() })
+      >(`${this.apiUrl}/admin/users/${userId}/toggle-status`, {}, { headers: this.getHeaders() })
       .pipe(
         tap((response) => {
           console.log(`Estado del usuario ${userId} actualizado:`, response);
