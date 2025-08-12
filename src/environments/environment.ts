29d5d99fa0b0558ca07e97280aedc76d09243280
ELVIS1230 <vladimirortiz1230@gmail.com>
2025-08-12 18:42:51 -0500

    feat: Arreglos enfermera

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // Entorno de desarrollo
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/api',
+  apiUrl: 'http://localhost:3000/api',
   appName: 'EHR System',
   version: '1.0.0',
 };
