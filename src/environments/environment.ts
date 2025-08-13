8d111342c5baa877fb3009e0fc919da1e1ea9ad9
arielGsDev <arielogavilanes1534@gmail.com>
2025-08-12 21:21:31 -0500

    feat: add generate treatment pdf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // Entorno de desarrollo
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/api',
+  apiUrl: 'http://46.202.144.33:3000/api',
   appName: 'EHR System',
   version: '1.0.0',
 };
